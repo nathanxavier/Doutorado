5a09037d5bc803fad7a0fffbb7152543b8f5ed07
Nathan Xavier <45743862+nathanxavier@users.noreply.github.com>
2022-02-09 11:53:19 -0300

    Commit 9/2

---
+++ Abordagem_01/Abord1.cpp
@@ -0,0 +1,5 @@
+#include "helper_3dmath.h"
+
+Quaternion q; // [w, x, y, z] quaternion container
+VectorFloat gravity; // [x, y, z] gravity vector
+float ypr[3]; // [yaw, pitch, roll] yaw/pitch/roll container and gravity vector
